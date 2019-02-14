5f299f96e82138d328a3cbb9eeaec76f03b0506b
Bartek <bartoszg@student.matnat.uio.no>
2019-02-14 23:18:43 +0100

    retta pa noen feil

--- task/task11.java
+++ task/task11.java
@@ -1,13 +1,12 @@
 import java.util.*;
-import java.util.Arrays.*;
 
-public class task11{
+public class task11 {
     public static void main(String[] args) {
 
         task11 t11 = new task11();
         int str = 8;
 
-        String [][] brett = new String[str][str];
+        String[][] brett = new String[str][str];
 
         for (int i = 0; i < str; i++) {
             for (int j = 0; j < str; j++) {
@@ -17,27 +16,28 @@ public class task11{
 
 
         t11.matprint(brett);
-        while(true){
+        while (true) {
             System.out.println("Skriv felt: ");
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
 
-            int [] pos = t11.translate(input);
+            int[] pos = t11.translate(input);
 
-            if(t11.checkPlacable(brett,str,pos[0],pos[1])){
+            System.out.println(pos[0]);
+            System.out.println(pos[1]);
+
+            if (t11.checkPlacable(brett, str, pos[0], pos[1])) {
                 brett[pos[0]][pos[1]] = "x";
             }
-
-
 
             t11.matprint(brett);
         }
     }
 
-    public void solve(String [][] brett, int str){
+    public void solve(String[][] brett, int str) {
         for (int i = 0; i < str; i++) {
             for (int j = 0; j < str; j++) {
-                if ((brett[j][i]).equals("o")){
+                if ((brett[j][i]).equals("o")) {
                     for (int k = 0; k < str; k++) {
 
                     }
@@ -46,17 +46,17 @@ public class task11{
         }
     }
 
-    public boolean checkPlacable(String [][] brett, int str, int row, int column){
-        // checks the whole row for the coordinate
+    public boolean checkPlacable(String[][] brett, int str, int row, int column) {
+        // checks the whole column for the coordinate
         for (int i = 0; i < str; i++) {
             //System.out.println(brett[i][column]);
-            if ((brett[i][column]).equals("x")){
+            if ((brett[i][column]).equals("x")) {
                 return false;
             }
         }
         // checks the whole row for the coordinate
         for (int i = 0; i < str; i++) {
-            if ((brett[row][i]).equals("x")){
+            if ((brett[row][i]).equals("x")) {
                 return false;
             }
         }
@@ -64,55 +64,80 @@ public class task11{
         // upright
         for (int i = 0; i < str; i++) {
 
-            if((row-i) < 0 || (column+i >= str)) break;
-            if((brett[row-i][column+i]).equals("x")){
+            if ((row - i) < 0 || (column + i >= str)) break;
+            if ((brett[row - i][column + i]).equals("x")) {
+                System.out.println("upright");
                 return false;
             }
         }
         //downleft
         for (int i = 0; i < str; i++) {
 
-            if((row+i) >= str || (column-i < 0)) break;
-            if((brett[row+i][column-i]).equals("x")){
+            if ((row + i) >= str || ((column - i) < 0)) break;
+
+            if ((brett[row + i][column - i]).equals("x")) {
+                System.out.println("downleft");
+                return false;
+            }
+        }
+
+        // upleft
+        for (int i = 0; i < str; i++) {
+
+            if ((row - i) < 0 || (column - i < 0)) break;
+
+            if ((brett[row - i][column - i]).equals("x")) {
+                System.out.println("upleft");
+                return false;
+            }
+        }
+
+        // downright
+        for (int i = 0; i < str; i++) {
+
+            if ((row + i) >= str || (column + i >= str)) break;
+
+            if ((brett[row + i][column + i]).equals("x")) {
+                System.out.println("downright");
                 return false;
             }
         }
         return true;
     }
 
-    public void matprint(String [][] brett){
-        for (String[] row : brett){
+    public void matprint(String[][] brett) {
+        for (String[] row : brett) {
             System.out.println(Arrays.toString(row));
         }
     }
 
-    public int [] translate(String pos){
-        String pos1 = pos.substring(0,1);
-        String pos2 = pos.substring(1,2);
+    public int[] translate(String pos) {
+        String pos2 = pos.substring(0, 1);
+        String pos1 = pos.substring(1, 2);
         int p1 = 9;
         int p2 = 9;
 
-        if(pos1.equals("a")) p1=7;
-        if(pos1.equals("b")) p1=6;
-        if(pos1.equals("c")) p1=5;
-        if(pos1.equals("d")) p1=4;
-        if(pos1.equals("e")) p1=3;
-        if(pos1.equals("f")) p1=2;
-        if(pos1.equals("g")) p1=1;
-        if(pos1.equals("h")) p1=0;
+        if (pos2.equals("a")) p2 = 0;
+        if (pos2.equals("b")) p2 = 1;
+        if (pos2.equals("c")) p2 = 2;
+        if (pos2.equals("d")) p2 = 3;
+        if (pos2.equals("e")) p2 = 4;
+        if (pos2.equals("f")) p2 = 5;
+        if (pos2.equals("g")) p2 = 6;
+        if (pos2.equals("h")) p2 = 7;
 
+        if (pos1.equals("1")) p1 = 7;
+        if (pos1.equals("2")) p1 = 6;
+        if (pos1.equals("3")) p1 = 5;
+        if (pos1.equals("4")) p1 = 4;
+        if (pos1.equals("5")) p1 = 3;
+        if (pos1.equals("6")) p1 = 2;
+        if (pos1.equals("7")) p1 = 1;
+        if (pos1.equals("8")) p1 = 0;
 
-        if(pos2.equals("1")) p2=0;
-        if(pos2.equals("2")) p2=1;
-        if(pos2.equals("3")) p2=2;
-        if(pos2.equals("4")) p2=3;
-        if(pos2.equals("5")) p2=4;
-        if(pos2.equals("6")) p2=5;
-        if(pos2.equals("7")) p2=6;
-        if(pos2.equals("8")) p2=7;
+        if (p1 > 8 || p2 > 8) return null;
+        if (p1 < 0 || p2 < 0) return null;
 
-        if(p1 > 8 || p2 > 8) return null;
-
-        return new int[] {p1,p2};
+        return new int[]{p1, p2};
     }
 }
